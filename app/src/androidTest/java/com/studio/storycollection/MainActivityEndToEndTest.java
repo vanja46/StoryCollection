f01cda15c7d229f9755fe878079f99003e85ce56
Vanja Buljevic <vanja.buljevic@gmail.com>
2023-09-09 20:45:40 +0200

    Zavrsni rad

--- app/src/androidTest/java/com/studio/storycollection/MainActivityEndToEndTest.java
+++ app/src/androidTest/java/com/studio/storycollection/MainActivityEndToEndTest.java
@@ -7,6 +7,7 @@ import androidx.test.filters.LargeTest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.core.app.ActivityScenario.launch;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
@@ -19,9 +20,9 @@ public class MainActivityEndToEndTest {
     @Test
     public void endToEnd1() {
         // End-to-End test for MainActivity
-        ActivityScenario<MainActivity> scenario = ActivityScenario.launch(MainActivity.class);
+        ActivityScenario<MainActivity> scenario = launch(MainActivity.class);
         onView(withText("Cinderella")).perform(click());
-        onView(withText("Cinderella\n Once upon a time, there was a kind girl named Cinderella. She lived with her wicked stepmother and two stepsisters who treated her poorly. With the help of her Fairy Godmother, she went to the royal ball and captured the prince's heart. At midnight, she rushed home, leaving behind a glass slipper. The prince found her through the slipper, and they lived happily ever after."))
+        onView(withText("Once upon a time, there was a kind girl named Cinderella. She lived with her wicked stepmother and two stepsisters who treated her poorly. With the help of her Fairy Godmother, she went to the royal ball and captured the prince's heart. At midnight, she rushed home, leaving behind a glass slipper. The prince found her through the slipper, and they lived happily ever after."))
                 .check(matches(isDisplayed()));
     }
 }
